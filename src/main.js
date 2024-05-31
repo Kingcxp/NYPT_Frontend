8dd2655dd86c3d585b5c67492dd4ba3640d55f46
Kingcq <404291187@qq.com>
2024-05-31 21:02:00 +0800

    Fix

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import ElementPlus from 'element-plus'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import axios from "@/utils/axiosInstance"
 
 const app = createApp(App)
