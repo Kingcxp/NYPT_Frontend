a9cb51ba48c7e938c7afd55ee4fa166428fe4816
Kingcq <404291187@qq.com>
2024-06-06 20:38:03 +0800

    Part off TeamCenter

--- src/views/TeamCenterView.vue
+++ src/views/TeamCenterView.vue
@@ -14,21 +14,24 @@ const active = ref(1)
     <el-container class="teamcenter-main-container">
       <el-aside class="teamcenter-aside">
         <el-menu default-active="1" :active="active" style="width: 15vw;">
-          <el-menu-item class="personal-menu-item" index="1" @click="active = 1">
-            <el-icon class="personal-menu-icon"><UserFilled /></el-icon>
-            <span class="personal-menu-text">队伍信息</span>
+          <el-container class="teamcenter-menu-title-container">
+            <label class="teamcenter-menu-title">功能列表</label>
+          </el-container>
+          <el-menu-item class="teamcenter-menu-item" index="1" @click="active = 1">
+            <el-icon><UserFilled /></el-icon>
+            <span class="teamcenter-menu-text">队伍信息</span>
           </el-menu-item>
-          <el-menu-item class="personal-menu-item" index="2" @click="active = 2">
-            <el-icon class="personal-menu-icon"><Checked /></el-icon>
-            <span class="personal-menu-text">比赛汇总</span>
+          <el-menu-item class="teamcenter-menu-item" index="2" @click="active = 2">
+            <el-icon><Checked /></el-icon>
+            <span class="teamcenter-menu-text">比赛汇总</span>
           </el-menu-item>
-          <el-menu-item class="personal-menu-item" index="3" @click="active = 3">
-            <el-icon class="personal-menu-icon"><StarFilled /></el-icon>
-            <span class="personal-menu-text">分数汇总</span>
+          <el-menu-item class="teamcenter-menu-item" index="3" @click="active = 3">
+            <el-icon><StarFilled /></el-icon>
+            <span class="teamcenter-menu-text">分数汇总</span>
           </el-menu-item>
-          <el-menu-item class="personal-menu-item" index="4" @click="active = 4">
-            <el-icon class="personal-menu-icon"><Opportunity /></el-icon>
-            <span class="personal-menu-text">奖项信息</span>
+          <el-menu-item class="teamcenter-menu-item" index="4" @click="active = 4">
+            <el-icon><Opportunity /></el-icon>
+            <span class="teamcenter-menu-text">奖项信息</span>
           </el-menu-item>
         </el-menu>
       </el-aside>
@@ -61,5 +64,24 @@ const active = ref(1)
   justify-content: center;
   align-items: center;
   flex-direction: column;
+}
+.teamcenter-menu-title-container {
+  display: flex;
+  justify-content: center;
+  padding: 2.5vh 0;
+}
+.teamcenter-menu-title {
+  margin-right: 3vw;
+  color: white;
+  font-size: x-large;
+  text-shadow: 0 0 12px rgba(255, 255, 255, 0.5);
+}
+.teamcenter-menu-item {
+  justify-content: center;
+}
+.teamcenter-menu-text {
+  margin-right: 1.5vw;
+  text-shadow: 0 0 12px rgba(255, 255, 255, 0.5);
+  font-size: large;
 }
 </style>
