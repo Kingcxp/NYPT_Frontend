8377a4a0c7b9980f8406474ebdace0d66970c855
Kingcq <404291187@qq.com>
2024-12-01 12:33:00 +0800

    feat: add scoring page design

--- src/components/AdminDashboard/Scoring.vue
+++ src/components/AdminDashboard/Scoring.vue
@@ -1,10 +1,40 @@
 <script setup>
+import { ref, onMounted } from 'vue'
+import { Delete, Finished } from '@element-plus/icons-vue'
+
+/*
+{
+  room_id: string,
+  round_id: string,
+  time: string,
+}
+should be sorted.
+*/
+const scoringData = ref([])
+
+onMounted(async () => {
+
+})
 </script>
 
 <template>
   <label class="admin-scoring-title">全部房间计分信息</label>
   <el-container class="admin-scoring-table-container">
-    
+    <el-table class="admin-scoring-table" :data="scoringData" stripe border>
+      <el-table-column fixed prop="room_id" label="房间号" width="120px"></el-table-column>
+      <el-table-column prop="round_id" label="轮次号" width="120px"></el-table-column>
+      <el-table-column prop="time" label="上传时间" width="280px"></el-table-column>
+      <el-table-column fixed="right" label="丢弃" width="72px">
+        <template #default="scope">
+          <el-button style="height: 25px; width: 25px;" type="danger" :icon="Delete" circle @click="removeFile(scope.$index)" />
+        </template>
+      </el-table-column>
+      <el-table-column fixed="right" label="合并" width="72px">
+        <template #default="scope">
+          <el-button style="height: 25px; width: 25px;" type="danger" :icon="Finished" circle @click="mergeFile(scope.$index)" />
+        </template>
+      </el-table-column>
+    </el-table>
   </el-container>
 </template>
 
@@ -23,5 +53,11 @@
   flex: none;
   height: auto;
   width: auto;
+}
+.admin-scoring-table {
+  width: fit-content;
+  max-width: 70vw;
+  text-shadow: 0 0 2px rgba(255, 255, 255, 0.5);
+  box-shadow: 0 0 8px rgba(255, 255, 255, 0.3);
 }
 </style>
